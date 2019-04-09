8c73c0ed89445c17e105016ad7c424190ba88bad
Évson Custódio Oliveira <evsoncustodiooli@gmail.com>
2019-04-09 19:00:06 -0300

    Fix "process.send" in Heroku

--- app/controllers/libs.js
+++ app/controllers/libs.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = () => {
     return {
         getThreeJS: (req, res) => {
-            process.send({id: process.env.id, cmd: 'notifyRequest'});
+            // process.send({id: process.env.id, cmd: 'notifyRequest'});
             fs.readFile(path.join(__dirname, '../../node_modules/three/build/three.js'), 'base64', (err, data) => {
                 res.status(200).send(data);
             });
--- app/controllers/objects.js
+++ app/controllers/objects.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = () => {
     return {
         getObjectByFilename: (req, res) => {
-            process.send({id: process.env.id, cmd: 'notifyRequest'});
+            // process.send({id: process.env.id, cmd: 'notifyRequest'});
             fs.readFile(path.join(__dirname, '../objects/', req.params.filename + '.js'), 'base64', (err, data) => {
                 res.status(200).send(data);
             });
